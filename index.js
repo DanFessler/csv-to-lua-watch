634e822c1daa67039771f196426ccc0cdcff7f86
Dan Fessler <fezzinate@gmail.com>
2020-08-14 01:48:59 -0700

    adding user prompts when params aren't supplied

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
 let fs = require("fs");
+const prompt = require("prompt");
 const Papa = require("papaparse");
 const { format, parse } = require("lua-json");
 
 // Make sure we got a filename on the command line.
 if (process.argv.length < 3) {
-  console.log("Usage: node " + process.argv[1] + " FILENAME");
-  process.exit(1);
+  // console.log("Usage: node " + process.argv[1] + " FILENAME");
+  prompt.message = null;
+  prompt.start();
+  prompt.get(
+    [
+      { name: "input", description: "Input Directory" },
+      { name: "output", description: "Output Directory (defaults to input)" }
+    ],
+    function(err, result) {
+      if (err) return console.log(err);
+      start(result.input, result.output || result.input);
+    }
+  );
+} else {
+  start(process.argv[2], process.argv[3] || process.argv[2]);
 }
 
-const watchDir = process.argv[2];
-const outputDir = process.argv[3] || watchDir;
+function start(watchDir, outputDir) {
+  // Convert entire directory
+  fs.readdir(watchDir, function(err, files) {
+    if (err) {
+      return console.log(err);
+    }
 
-// Convert entire directory
-fs.readdir(watchDir, function(err, files) {
-  if (err) {
-    return console.log(err);
-  }
-
-  files.forEach(function(filename) {
-    convertAndSave(filename);
+    files.forEach(function(filename) {
+      convertAndSave(filename, watchDir, outputDir);
+    });
   });
-});
 
-// Continue to watch for changes
-fs.watch(watchDir, function(eventType, filename) {
-  convertAndSave(filename);
-});
+  // Continue to watch for changes
+  fs.watch(watchDir, function(eventType, filename) {
+    convertAndSave(filename, watchDir, outputDir);
+  });
+}
 
 function convertText(text) {
   return format(Papa.parse(text, { header: true }).data);
 }
 
-function convertAndSave(filename) {
+function convertAndSave(filename, watchDir, outputDir) {
   if (filename.substr(filename.length - 4) === ".csv") {
     const csvText = fs.readFileSync(watchDir + "\\" + filename, "utf8");
     const newPath =
